013f212ef0225ba989a3c2ab3aeeed94166097c9
Edvard Sørby <edvarso@stud.ntnu.no>
2022-05-23 23:37:30 +0200

    Improved program styling.

--- src/main/java/edu/ntnu/idatt2001/edvarso/Application.java
+++ src/main/java/edu/ntnu/idatt2001/edvarso/Application.java
@@ -3,8 +3,10 @@ package edu.ntnu.idatt2001.edvarso;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.net.URL;
 
 public class Application extends javafx.application.Application {
@@ -24,7 +26,7 @@ public class Application extends javafx.application.Application {
             Scene scene = new Scene(root);
 
             stage.setTitle("Wargames");
-            //stage.getIcons().add(new Image(new FileInputStream("src/main/resources/images/icon.png")));
+            stage.getIcons().add(new Image(new FileInputStream("src/main/resources/edu/ntnu/idatt2001/edvarso/styles/icon.png")));
             stage.setScene(scene);
             stage.setMaximized(true);
             stage.show();
